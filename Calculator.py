b4668601738560d0f46562aa875a8bc7f54f5625
Arlind <arlindshaqiri@hotmail.com>
2022-07-06 01:10:41 +0200

    second push

--- Calculator.py
+++ Calculator.py
@@ -3,4 +3,5 @@
 a=int(input("Type the first number: "))
 b=int(input("Type the second number: "))
 
-print (f"The sum of the first number and the second numer is:  {a+b}")
+
+print (f"The sum of {a} and {b} is:  {a+b}")
